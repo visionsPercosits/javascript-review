bc78cae33b8bad06b5715583cc6acdfb1eb38895
Victor <we5705988@gmail.com>
2025-02-03 21:11:51 -0300

    Aprendendo a verificar propriedades

--- Objetos/Propriedades/properties.js
+++ Objetos/Propriedades/properties.js
@@ -12,3 +12,21 @@ if (book && book['sub-title']) {
 
 console.log(book);
 
+// Verificando propriedades (Métodos nativos)
+
+console.log("toString" in book);
+console.log(['title'] in book);
+
+console.log(book.hasOwnProperty(['title']));
+
+console.log(book.propertyIsEnumerable(['title']));
+
+
+// Enumerando propriedades
+
+for (p in book) {
+  if(!book.propertyIsEnumerable(p)) {
+    continue;
+  }
+  console.log(p);
+}
