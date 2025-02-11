2df16e56e7a468295efeee7950719390678520f5
Victor <we5705988@gmail.com>
2025-02-11 20:38:31 -0300

    Pequeno ajuste do meu exercício

--- Classes/exercicio03.js
+++ Classes/exercicio03.js
@@ -1,17 +1,27 @@
 class Pessoa {
-  constructor() {
-    this.nome = nome,
-    this.idade = idade
+  constructor(nome, idade) {
+    this.nome = nome;
+    this._idade = idade;  
+  }                      
+  /*
+      Deixando a propriedade privada, (Caso eu queira que o usuário digite o valor dessa propriedade e passe pelo getter
+      É necessário deixar ela privada utilizando _ assim ela irá passar pela verificação do setter e getter primeiro.
+  */
+
+
+  get idade() {
+    return this._idade;
   }
 
-  get blockNegativeNumber() {
-    if (this.idade < 1) {
-      return 'Sem números negativos!'
-    }
+  set idade(value) {
+    if (value < 0) {
+      console.log('Idade inválida.');
+    }    
   }
 }
 
-const pessoa = new Pessoa('Victor', 20);
+const pessoa = new Pessoa('Victor', 25);
 
 pessoa.idade = -2;
+
 console.log(pessoa.idade);
