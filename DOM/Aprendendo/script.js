89418a6835ee37fe0ab70390206cc0fa8cff0b35
Victor <we5705988@gmail.com>
2025-02-14 20:16:36 -0300

    Aprendendo DOM

---
+++ DOM/Aprendendo/script.js
@@ -0,0 +1,5 @@
+const body = document.body;
+const div = document.createElement('div');
+
+div.innerText = 'Hello World!';
+body.append(div);
---
+++ DOM/Exercícios/script.js
@@ -0,0 +1,14 @@
+const body = document.body;
+
+const p = document.createElement('p');
+p.innerText = 'Este é um novo parágrafo!';
+
+body.append(p);
+
+// Exercício #2
+
+const titulo = document.getElementById('titulo');
+titulo.innerText = 'Texto alterado!';
+
+// Exercício #3
+
